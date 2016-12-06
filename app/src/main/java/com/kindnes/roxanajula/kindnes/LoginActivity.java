d19179545aaca1bca06b94f3cee1ddb04f302345
Roxana Jula <roxana.jula@gmail.com>
2016-12-06 19:33:20 +0100

    Initial commit

---
+++ app/src/main/java/com/kindnes/roxanajula/kindnes/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.kindnes.roxanajula.kindnes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int RC_SIGN_IN= 0;
+    private FirebaseAuth auth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            //User already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+            Intent homepage = new Intent(LoginActivity.this, HomeActivity.class);
+            startActivity(homepage);
+        } else {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(
+                            AuthUI.FACEBOOK_PROVIDER,
+                            AuthUI.GOOGLE_PROVIDER)
+                    .build(), RC_SIGN_IN);
+        }
+        findViewById(R.id.log_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                //User logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail() + auth.getCurrentUser().getDisplayName() + auth.getCurrentUser().getPhotoUrl());
+                Intent homepage = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(homepage);
+            }
+            else {
+                //User not authenticated
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.log_out_button) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "USER LOGGED OUT");
+                            finish();
+                        }
+                    });
+
+        }
+    }
+}
