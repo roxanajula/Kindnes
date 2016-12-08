eaf9d1290134f490ab132e3e63ea642971ea6f49
Roxana Jula <roxana.jula@gmail.com>
2016-12-08 02:04:42 +0100

    Home page with menu drawer; Initial work on the map view;

--- app/src/main/java/com/kindnes/roxanajula/kindnes/HomeActivity.java
+++ app/src/main/java/com/kindnes/roxanajula/kindnes/HomeActivity.java
@@ -1,13 +1,90 @@
 package com.kindnes.roxanajula.kindnes;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        TextView userNameMenu = (TextView) navigationView.getHeaderView(0).findViewById(R.id.userNameMenu);
+        userNameMenu.setText(user.getDisplayName());
+        ImageView userPhotoMenu = (ImageView) navigationView.getHeaderView(0).findViewById(R.id.userPhotoMenu);
+        Picasso.with(this).load(user.getPhotoUrl()).into(userPhotoMenu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the home action
+        } else if (id == R.id.nav_about) {
+
+        } else if (id == R.id.nav_how_it_works) {
+
+        } else if (id == R.id.nav_terms) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void goToFoodView(View view)
+    {
+        Intent intent = new Intent(HomeActivity.this, FoodActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/kindnes/roxanajula/kindnes/LoginActivity.java
+++ app/src/main/java/com/kindnes/roxanajula/kindnes/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.kindnes.roxanajula.kindnes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
